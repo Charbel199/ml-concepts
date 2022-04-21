0d97f0f3946c4711285149cdce839b4ef80f3651
Charbel199 <charbel-boumaroun@outlook.com>
2022-04-21 23:37:17 +0300

    Add comments to simple gan

--- models/GAN/SimpleGAN.py
+++ models/GAN/SimpleGAN.py
@@ -82,21 +82,22 @@ for epoch in range(epochs):
         ## Train discriminator, maximize (log(D(real)) + log(1-D(G(z))))
         # log(D(real)) part
         discriminator_real = discriminator(real_image).view(-1)
-        loss_discriminator_real = criterion(discriminator_real, torch.ones_like(discriminator_real))
+        loss_discriminator_real = criterion(discriminator_real, torch.ones_like(discriminator_real)) # Discriminator should associate real image with  1
         # log(1-D(G(z)) part
         discriminator_fake = discriminator(fake_image).view(-1)
-        loss_discriminator_fake = criterion(discriminator_fake, torch.zeros_like(discriminator_fake))
+        loss_discriminator_fake = criterion(discriminator_fake, torch.zeros_like(discriminator_fake)) # Discriminator should associate fake image with  0
         # Combined loss
         loss_discriminator = (loss_discriminator_real + loss_discriminator_fake) / 2
 
         discriminator.zero_grad()
+        # loss.backward() sets the grad attribute of all tensors with requires_grad=True in the computational graph
         loss_discriminator.backward(retain_graph=True)  # retrain_graph so that what was used in this pass is not cleared from cache, ex: fake_image
         opt_discriminator.step()
 
         ## Train generator, minimize log(1-D(G(z))) OR maximize log(D(G(z)))
         # log(D(G(z)) part
         discriminator_fake = discriminator(fake_image).view(-1)
-        loss_generator = criterion(discriminator_fake, torch.ones_like(discriminator_fake))
+        loss_generator = criterion(discriminator_fake, torch.ones_like(discriminator_fake)) # Generator wants Discriminator to associate fake image with  1
 
         generator.zero_grad()
         loss_generator.backward(retain_graph=True)  # retrain_graph so that what was used in this pass is not cleared from cache, ex: fake_image
