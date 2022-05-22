76b6da7a3dda4e562082226896e70610217e903e
Charbel199 <charbel-boumaroun@outlook.com>
2022-05-22 17:01:00 +0100

    Add Unet torch code

---
+++ models/UNet/UNet.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+
+class DoubleConvolution(nn.Module):
+    def __int__(self, in_channels, out_channels):
+        super(DoubleConvolution, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),  # Padding 1, Same convolution
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),  # Padding 1, Same convolution
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNET(nn.Module):
+    def __int__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part
+        for feat in features:
+            self.downs.append(DoubleConvolution(in_channels, feat))
+            in_channels = feat
+
+        # Up part
+        for feat in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feat * 2, feat, kernel_size=2, stride=2))
+            self.ups.append(DoubleConvolution(feat * 2, feat))
+
+        self.bottleneck = DoubleConvolution(features[-1], features[-1] * 2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = reversed(skip_connections)
+        for index in range(0, len(self.ups), 2):
+            x = self.ups[index](x)
+            skip_connection = skip_connections[index // 2]
+            concatenate_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[index + 1](concatenate_skip)
+
+        return self.final_conv(x)
