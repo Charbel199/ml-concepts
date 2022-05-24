f971c547f572876c8c3ca492762f4d494f6bfa6c
Charbel199 <charbel-boumaroun@outlook.com>
2022-05-24 11:43:00 +0100

    Fix UNET bugs

--- models/UNet/UNet.py
+++ models/UNet/UNet.py
@@ -4,8 +4,8 @@ import torchvision.transforms.functional as TF
 
 
 class DoubleConvolution(nn.Module):
-    def __int__(self, in_channels, out_channels):
-        super(DoubleConvolution, self).__init__()
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),  # Padding 1, Same convolution
             nn.BatchNorm2d(out_channels),
@@ -19,9 +19,12 @@ class DoubleConvolution(nn.Module):
         return self.conv(x)
 
 
+
 class UNET(nn.Module):
-    def __int__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512]):
-        super(UNET, self).__init__()
+    def __init__(self, in_channels=3, out_channels=1, features=None):
+        super().__init__()
+        if features is None:
+            features = [64, 128, 256, 512]
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -47,14 +50,26 @@ class UNET(nn.Module):
             x = self.pool(x)
 
         x = self.bottleneck(x)
-        skip_connections = reversed(skip_connections)
+        skip_connections = skip_connections[::-1]
         for index in range(0, len(self.ups), 2):
             x = self.ups[index](x)
             skip_connection = skip_connections[index // 2]
 
             if x.shape != skip_connection.shape:
-                x = TF.resize(x, size=skip_connection.shape[2:]) # Just getting height and width
+                x = TF.resize(x, size=skip_connection.shape[2:])  # Just getting height and width
             concatenate_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[index + 1](concatenate_skip)
 
         return self.final_conv(x)
+
+
+def run():
+    x = torch.randn((3, 1, 160, 160))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+
+
+if __name__ == "__main__":
+    run()
